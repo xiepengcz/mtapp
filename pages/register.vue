caee92f68fc8aa5f816f5d67cc234f521c7bd587
jieyouzi <949026370@qq.com>
2020-05-24 03:08:10 +0800

    开始编写注册页面

---
+++ pages/register.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page_register">
+    <article class="header"></article>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'blank'
+}
+</script>
+
+<style>
+</style>
